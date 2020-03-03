f4ec1c484556b05f92126b2975601d2116013081
konstantinosrizos <rizos.dev@gmail.com>
2020-03-03 10:43:04 +0000

    Initial Commit

---
+++ FBSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FBSwiftUI
+//
+//  Created by Konstantinos Rizos on 3/3/20.
+//  Copyright © 2020 Konstantinos Rizos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
